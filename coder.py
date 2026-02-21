460ea8a8fa2cbfa9a69f10b0abf92cbe808eac71
Andon-LaFreniere <andonlafreniere2706@gmail.com>
2026-02-21 11:13:14 -0500

    add gitignore and update coder.py to include OpenAI API call

--- coder.py
+++ coder.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
 """Make a call to OpenAI API to generate code based on the requirement"""
 def generate_tool_code(requirement: str)->str:
-    client = OpenAI();
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"));
 
